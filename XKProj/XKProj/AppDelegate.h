af18dd63aa3b6f5fbcb897869f091a61ae1af80e
kangbo <lala.kang1989@gmail.com>
2017-11-14 18:28:08 +0800

    ADD:添加源码和忽略文件

---
+++ XKProj/XKProj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XKProj
+//
+//  Created by meishi on 14/11/2017.
+//  Copyright © 2017 Kangbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XKProj/XKProj/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XKProj
+//
+//  Created by meishi on 14/11/2017.
+//  Copyright © 2017 Kangbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
